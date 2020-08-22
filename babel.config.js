2a9d2656fb372d49c2797774653ba1121ee2b842
Marco <marco_1891@163.com>
2020-08-22 10:23:34 +0800

    feat: remove console setting

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,10 @@
+const plugins = []
+if (process.env.NODE_ENV === 'production') {
+  plugins.push('transform-remove-console')
+}
+
 module.exports = {
+  plugins: plugins,
   presets: [
     '@vue/app'
   ]
